48a5427b14a81d0757205cda074c156a1e168074
Imperor <imperor0103@gmail.com>
2025-03-12 17:09:40 +0900

    [WIP] 작업할 스크립트, 씬

---
+++ Assets/Scripts/Buildings/WorkTool.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkTool : TestItems
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
