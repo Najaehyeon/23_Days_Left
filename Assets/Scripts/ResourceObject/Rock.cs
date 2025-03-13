ae16b6eb6b3ca06e05b6627d8eb5430890f96cce
Najaehyeon <jaehyeon4832@gmail.com>
2025-03-13 14:07:47 +0900

    feat: "환경 오브젝트 생성 및 파밍 기능 추가"

---
+++ Assets/Scripts/ResourceObject/Rock.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : ResourceObject
+{
+    private void Update()
+    {
+        RespawnTree();
+    }
+
+    private void OnCollisionEnter(Collision collision)  // ÇÃ·¹ÀÌ¾î°¡ Ä¶ ¶§
+    {
+        if (collision.gameObject.CompareTag("ÇÃ·¹ÀÌ¾î ¹«±â"))
+        {
+            if (remainDigCount >= 0)
+            {
+                Instantiate(dropResource, transform.position + Vector3.up, Quaternion.identity);
+                remainDigCount--;
+                // ±ø±ø ¼Ò¸®
+            }
+            else 
+            {
+                // Æ½Æ½ ¼Ò¸®
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ResourceObject/Tree.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using UnityEngine;
+
+public class Tree : ResourceObject
+{
+    private bool isObjectAtPosition;
+
+    BoxCollider _boxCollider;
+    MeshRenderer _meshRenderer;
+    Animator _animator;
+
+    private void Awake()
+    {
+        _boxCollider = GetComponent<BoxCollider>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        RespawnTree();
+    }
+
+    public override void RespawnTree()
+    {
+        if (_dayNightCycle.currentTime == 0.4f && !isObjectAtPosition)
+        {
+            _meshRenderer.enabled = true;
+            _boxCollider.isTrigger = false;
+            remainDigCount = digCount;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)  // 플레이어가 캘 때
+    {
+        if (collision.gameObject.CompareTag("플레이어 무기"))
+        {
+            if (remainDigCount > 0)
+            {
+                Instantiate(dropResource, transform.position + Vector3.up, Quaternion.identity);
+                remainDigCount--;
+            }
+            else if (remainDigCount == 0)
+            {
+                Instantiate(dropResource, transform.position + Vector3.up, Quaternion.identity);
+                _animator.enabled = true;       // 넘어지는 애니메이션 실행
+                _boxCollider.isTrigger = true;  // 
+                StartCoroutine(TreeDown());
+            }
+        }
+    }
+
+    IEnumerator TreeDown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        // 메쉬 비활성화
+        _meshRenderer.enabled = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("플레이어가 설치한 오브젝트"))
+        {
+            isObjectAtPosition = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("플레이어가 설치한 오브젝트"))
+        {
+            isObjectAtPosition = false;
+        }
+    }
+}
