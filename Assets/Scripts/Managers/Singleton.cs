0b45fb5f2bb6047ad3e38989c92a6bfc155a31ce
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-03-13 19:23:38 +0900

    Chore: merge

--- Assets/Scripts/Managers/Singleton.cs
+++ Assets/Scripts/Managers/Singleton.cs
@@ -1,38 +1,43 @@
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+namespace _23DaysLeft.Managers
 {
-    private static T instance;
-    public static T Instance
+
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        get
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (!instance)
+                {
+                    instance = FindObjectOfType<T>();
+                    if (!instance)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.name = typeof(T).Name;
+                        instance = obj.AddComponent<T>();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public virtual void Awake()
         {
             if (!instance)
             {
-                instance = FindObjectOfType<T>();
-                if (!instance)
-                {
-                    GameObject obj = new GameObject();
-                    obj.name = typeof(T).Name;
-                    instance = obj.AddComponent<T>();
-                }
+                instance = this as T;
+                DontDestroyOnLoad(gameObject);
             }
-
-            return instance;
-        }
-    }
-
-    public virtual void Awake()
-    {
-        if (!instance)
-        {
-            instance = this as T;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            if (instance != this)
-                Destroy(gameObject);
+            else
+            {
+                if (instance != this)
+                    Destroy(gameObject);
+            }
         }
     }
 }
