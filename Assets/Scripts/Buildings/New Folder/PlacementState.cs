b045cce58e8b3340c8bfba9bf86862e09702e9d6
Imperor <imperor0103@gmail.com>
2025-03-17 04:36:12 +0900

    [Fix] 격자가 맞지 않는 문제 해결, 나무 위에 건축 가능

--- Assets/Scripts/Buildings/New Folder/PlacementState.cs
+++ Assets/Scripts/Buildings/New Folder/PlacementState.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using static Unity.VisualScripting.Member;
 
+/// <summary>
+/// 나무 위에는 건물을 지을 수 있다는 설정
+/// </summary>
 public class PlacementState : IBuildingState
 {
     private int selectedObjIndex = -1;
@@ -10,8 +13,8 @@ public class PlacementState : IBuildingState
     Grid grid;
     PreviewSystem previewSystem;
     ObjectsDatabaseSO database;
-    GridData floorData;
-    GridData furnitureData;
+    GridData tree;  // 나무 위에는 빌딩을 지을 수 있다
+    GridData buildingData;
     ObjectPlacer objectPlacer;
     SoundFeedBack soundFeedback;
 
@@ -19,8 +22,8 @@ public class PlacementState : IBuildingState
                           Grid grid,
                           PreviewSystem previewSystem,
                           ObjectsDatabaseSO database,
-                          GridData floorData,
-                          GridData furnitureData,
+                          GridData treeData,
+                          GridData buildingData,
                           ObjectPlacer objectPlacer,
                           SoundFeedBack soundFeedback)
     {
@@ -28,8 +31,8 @@ public class PlacementState : IBuildingState
         this.grid = grid;
         this.previewSystem = previewSystem;
         this.database = database;
-        this.floorData = floorData;
-        this.furnitureData = furnitureData;
+        this.tree = treeData;
+        this.buildingData = buildingData;
         this.objectPlacer = objectPlacer;
         this.soundFeedback = soundFeedback;
 
@@ -63,7 +66,7 @@ public class PlacementState : IBuildingState
         int index = objectPlacer.PlaceObject(database.objectsData[selectedObjIndex].Prefab, grid.CellToWorld(gridPosition));
 
         // 이 객체의 인덱스를 데이터에 추가
-        GridData selectedData = database.objectsData[selectedObjIndex].ID == 0 ? floorData : furnitureData;
+        GridData selectedData = database.objectsData[selectedObjIndex].ID == 0 ? tree : buildingData;   // 나무와 건물을 구분한다
         selectedData.AddObjectAt(gridPosition,
             database.objectsData[selectedObjIndex].Size,
             database.objectsData[selectedObjIndex].ID,
@@ -74,7 +77,7 @@ public class PlacementState : IBuildingState
 
     private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjIndex)
     {
-        GridData selectedData = database.objectsData[selectedObjIndex].ID == 0 ? floorData : furnitureData;
+        GridData selectedData = database.objectsData[selectedObjIndex].ID == 0 ? tree : buildingData;
         return selectedData.CanPlaceObjAt(gridPosition, database.objectsData[selectedObjIndex].Size);
     }
     public void UpdateState(Vector3Int gridPosition)
--- Assets/Scripts/Buildings/New Folder/PlacementSystem.cs
+++ Assets/Scripts/Buildings/New Folder/PlacementSystem.cs
@@ -9,13 +9,10 @@ using UnityEngine;
 /// </summary>
 public class PlacementSystem : MonoBehaviour
 {
-    //[SerializeField] private GameObject mouseIndicator; // 선택하는 위치를 시각화(작은 Sphere 프리팹 연결해서 커서 위치를 보여준다)
-    //[SerializeField] private GameObject cellIndicator; // 마우스가 해당 격자 안에 있음을 표시(마우스 커서를 따라이동)
     [SerializeField] private TempInputManager inputManager;   // 마우스
     [SerializeField] private Grid grid;   // 그리드 컴포넌트
 
     [SerializeField] private ObjectsDatabaseSO database;
-    //private int selectedObjIndex = -1;   // -1: 오브젝트가 선택되지 않았다
 
     [SerializeField] private GameObject gridVisualization;
 
@@ -25,9 +22,6 @@ public class PlacementSystem : MonoBehaviour
 
     private GridData floorData, furnitureData;
 
-    //private Renderer previewRenderer;
-
-    //private List<GameObject> placedGameObjects = new List<GameObject>(); // 생성된 게임오브젝트 저장
 
     [SerializeField] private PreviewSystem preview;
 
@@ -45,10 +39,6 @@ public class PlacementSystem : MonoBehaviour
         StopPlacement();
         floorData = new GridData();
         furnitureData = new GridData();
-
-        //previewRenderer = cellIndicator.GetComponentInChildren<Renderer>();
-
-
     }
     public void StartPlacement(int ID)
     {
@@ -74,7 +64,6 @@ public class PlacementSystem : MonoBehaviour
         {
             return;
         }
-        // 커서가 UI위에 있지 않다면(그리드 위에 있다면)
         Vector3 mousePos = inputManager.GetSelectedMapPos(); // 마우스로 선택한 위치 가져온다
         Vector3Int gridPosition = grid.WorldToCell(mousePos);   // 마우스가 있는 위치를 3d로 변환하여 그리드의 어느 격자 내에 있는지 알아낸다
 
@@ -115,12 +104,6 @@ public class PlacementSystem : MonoBehaviour
         // grid 내에서 커서가 이동하면 연산하지 않는다
         if (lastDetectedPosition != gridPosition)
         {
-            //bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjIndex);
-            ////previewRenderer.material.color = placementValidity ? Color.green : Color.red;
-
-            //mouseIndicator.transform.position = mousePos;
-            ////cellIndicator.transform.position = grid.CellToWorld(gridPosition); // 격자 오브젝트를 그 좌표로 이동
-            //preview.UpdatePosition(grid.CellToWorld(gridPosition), placementValidity);
             buildingState.UpdateState(gridPosition);
 
             lastDetectedPosition = gridPosition;
