e45c0e061a8c0887abf33dc70b394549eb3fbbb5
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-03-18 12:08:50 +0900

    WIP

--- Assets/Scripts/Monsters/OnGui.cs
+++ Assets/Scripts/Monsters/OnGui.cs
@@ -9,24 +9,6 @@ namespace _23DaysLeft.Utils
     {
         public bool isDebug = true;
         [SerializeField] private CreatureController creature;
-        public List<CreatureController> creatures = new List<CreatureController>();
-
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                Cursor.lockState = CursorLockMode.None;
-            }
-            
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                for (int i = 0; i < creatures.Count; i++)
-                {
-                    creatures[i].OnHit(20f);
-                }
-            }
-        }
-
         public void OnGUI()
         {
             if (!isDebug) return;
